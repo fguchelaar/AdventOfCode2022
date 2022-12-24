f57612d7b52bf2abfb3aadd6aa49857ee1bb2d8b
Frank Guchelaar <fguchelaar@gmail.com>
2022-12-24 21:51:38 +0100

    solved day 16
    code is not something to be proud of, but it get's the job done

--- day-16-proboscidea-volcanium/README.md
+++ day-16-proboscidea-volcanium/README.md
@@ -1,3 +1,9 @@
 # 16 [Proboscidea Volcanium]
 
 https://adventofcode.com/2022/day/16
+
+The first part is quickly solved in about half a second. Part two is not to be
+spoken about... it get's the job done, but that's about it
+
+## Graph layout of the rooms:
+![room layout](../Resources/aoc202216.png)
--- day-16-proboscidea-volcanium/Sources/Puzzle/Puzzle.swift
+++ day-16-proboscidea-volcanium/Sources/Puzzle/Puzzle.swift
@@ -46,6 +46,10 @@ class Puzzle {
         return distance
     }
 
+    func valve(for name: String) -> Valve {
+        valves.first { $0.name == name }!
+    }
+
     func part1() -> Int {
         let distanceMaps: [String: [Valve: Int]]
         // Create distance maps for all valves that have flow
@@ -100,11 +104,102 @@ class Puzzle {
         }
     }
 
-    func valve(for name: String) -> Valve {
-        valves.first { $0.name == name }!
+    func part2() -> Int {
+        let distanceMaps: [String: [Valve: Int]]
+        // Create distance maps for all valves that have flow
+        distanceMaps = (valves
+            .filter { $0.flow > 0 || $0.name == "AA" })
+            .reduce(into: [String: [Valve: Int]]()) { maps, valve in
+                maps[valve.name] = distanceMap(from: valve, in: valves)
+            }
+
+        solvePart2(start1: valve(for: "AA"), start2: valve(for: "AA"), distanceMaps: distanceMaps, visited: [], pressure: 0, ttl1: 26, ttl2: 26)
+        return maxPart2
     }
 
-    func part2() -> Int {
-        3
+    var maxPart2 = Int.min
+
+    func solvePart2(start1: Valve, start2: Valve, distanceMaps: [String: [Valve: Int]], visited: [Valve], pressure: Int, ttl1: Int, ttl2: Int) {
+        // ran out of time
+        if ttl1 <= 0 && ttl2 <= 0 {
+            maxPart2 = max(maxPart2, pressure)
+            return
+        }
+
+        let distanceMap1 = distanceMaps[start1.name]!
+        let distanceMap2 = distanceMaps[start2.name]!
+
+        let toVisit1 = valves.filter {
+            $0.flow > 0
+                && !visited.contains($0)
+                && distanceMap1[$0]! < ttl1
+        }
+
+        let toVisit2 = valves.filter {
+            $0.flow > 0
+                && !visited.contains($0)
+                && distanceMap2[$0]! < ttl2
+        }
+
+        let potentialMaxPressure1 = toVisit1.reduce(0) {
+            $0 + (ttl1 - distanceMap1[$1]! - 1) * $1.flow
+        }
+        let potentialMaxPressure2 = toVisit2.reduce(0) {
+            $0 + (ttl2 - distanceMap2[$1]! - 1) * $1.flow
+        }
+
+        if pressure + potentialMaxPressure1 + potentialMaxPressure2 < maxPart2 {
+            return
+        }
+
+        // ran out of valves to visit
+        if toVisit1.isEmpty && toVisit2.isEmpty {
+            maxPart2 = max(maxPart2, pressure)
+            return
+        }
+
+        if toVisit1.isEmpty {
+            toVisit2.forEach { next2 in
+                let nextDistance2 = distanceMap2[next2]!
+                let nextPressure2 = next2.flow * (ttl2 - nextDistance2 - 1)
+                solvePart2(start1: start1,
+                           start2: next2,
+                           distanceMaps: distanceMaps,
+                           visited: visited + [next2],
+                           pressure: pressure + nextPressure2,
+                           ttl1: ttl1,
+                           ttl2: ttl2 - nextDistance2 - 1)
+            }
+        } else if toVisit2.isEmpty {
+            toVisit1.forEach { next1 in
+                let nextDistance1 = distanceMap1[next1]!
+                let nextPressure1 = next1.flow * (ttl1 - nextDistance1 - 1)
+                solvePart2(start1: next1,
+                           start2: start2,
+                           distanceMaps: distanceMaps,
+                           visited: visited + [next1],
+                           pressure: pressure + nextPressure1,
+                           ttl1: ttl1 - nextDistance1 - 1,
+                           ttl2: ttl2)
+            }
+        } else {
+            toVisit1.forEach { next1 in
+                let nextDistance1 = distanceMap1[next1]!
+                let nextPressure1 = next1.flow * (ttl1 - nextDistance1 - 1)
+
+                toVisit2.filter { $0 != next1 }.forEach { next2 in
+                    let nextDistance2 = distanceMap2[next2]!
+                    let nextPressure2 = next2.flow * (ttl2 - nextDistance2 - 1)
+
+                    solvePart2(start1: next1,
+                               start2: next2,
+                               distanceMaps: distanceMaps,
+                               visited: visited + [next1, next2],
+                               pressure: pressure + nextPressure1 + nextPressure2,
+                               ttl1: ttl1 - nextDistance1 - 1,
+                               ttl2: ttl2 - nextDistance2 - 1)
+                }
+            }
+        }
     }
 }
